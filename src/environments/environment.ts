35abd47a64417b2a932e02a9d85033afeeaba261
Diego Perez <dapohp@gmail.com>
2021-06-01 18:00:38 -0400

    adding the steps to do made the votes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
    api: "http://localhost:3000/",
-  //api: "https://elecciones-backend.herokuapp.com/"
+   //api: "https://elecciones-backend.herokuapp.com/"
 };
 
 /*
